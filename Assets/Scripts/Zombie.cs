0ed0325877fb582002d2d8ea3a6088c4a91ee5a3
RyanHissi <158734173+RyanHissi@users.noreply.github.com>
2024-02-21 16:17:03 +0100

    Monsters chasing.
    The zombie is now able to chase the player.

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class obunga : MonoBehaviour
+{
+    public NavMeshAgent ai;
+    public Transform player;
+    Vector3 dest;
+
+    void Update()
+    {
+        dest = player.position;
+        ai.destination = dest;
+    }
+}
+
